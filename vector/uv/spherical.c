05411b1441e7e19ddd9bf6f4a7e3da9b5c01951b
Map <blendfan@gmail.com>
2017-06-12 00:23:57 +0200

    fix uv spherical coord

--- vector/uv/spherical.c
+++ vector/uv/spherical.c
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/11 17:39:17 by qloubier          #+#    #+#             */
-/*   Updated: 2017/06/11 18:22:32 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/06/12 00:15:45 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,12 @@ t_v2f		nortouv(t_v3f nor)
 	float	b;
 
 	v = (t_v3f){nor.x, nor.y, 0.0};
-	if (v.z >= 1.0f)
+	if ((nor.z >= 1.0f) || (nor.z <= -1.0f))
 		v = (t_v3f){0.0, 1.0, 0.0};
 	normalize3f(&v);
-	a = acosf(v.y) / (float)M_PI;
-	if (v.x > 0.0f)
-		a = 0.5f + (a / 2.0f);
-	else
-		a = 0.5f - (a / 2.0f);
+	a = acosf(v.y) / (float)(M_PI * 2);
+	if (v.x < 0.0f)
+		a = 1.0f - a;
 	b = acosf(nor.z) / (float)M_PI;
 	return ((t_v2f){a,b});
 }
