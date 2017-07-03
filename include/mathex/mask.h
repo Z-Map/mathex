84a05ad4b08f84426c927209b7ac6d5f3090afbb
Map <blendfan@gmail.com>
2017-07-04 01:30:17 +0200

    add mask define

--- include/mathex/mask.h
+++ include/mathex/mask.h
@@ -6,14 +6,29 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/17 16:54:36 by qloubier          #+#    #+#             */
-/*   Updated: 2016/02/17 16:56:01 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/07/04 01:09:36 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MASK_H
 # define MASK_H
 
-# define MSIGN (unsigned int)0x80000000
-# define MUSIGN (unsigned int)0x7FFFFFFF
+# define MSIGN_C	0x80u
+# define MUSIGN_C	0x7Fu
+
+# define MSIGN_S	0x8000u
+# define MUSIGN_S	0x7FFFu
+
+# define MSIGN		(unsigned int)0x80000000
+# define MUSIGN		(unsigned int)0x7FFFFFFF
+
+# define MSIGN_L	0x8000000000000000ul
+# define MUSIGN_L	0x7FFFFFFFFFFFFFFFul
+
+# define FLOAT_EXP	0x7f800000u
+# define FLOAT_MAN	0x007fffffu
+
+# define DOUBLE_EXP	0x7ff0000000000000ul
+# define DOUBLE_MAN	0x000ffffffffffffful
 
 #endif
